3847874c6fc357f33cd5bcef49e972e9c2173d38
Snake <stop@mail.ru>
2023-12-17 20:18:38 +0300

    task4_end

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Metadata.Ecma335;
+
+string[] array = {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+
+int count =0;
+
+for(int rows = 0; rows < array.Length; rows++) {
+
+    if(array[rows].Length<4) count++;
+}
+
+
+Function(count, array);
+
+static void Function(int count, string[] array) {    
+
+string[] newarray = new string[count];
+
+int row=0;
+for(int rows = 0; rows < array.Length; rows++) {
+
+    if(array[rows].Length<4) {
+        newarray[row]=array[rows];
+        row++;
+    };
+}
+Console.Write("[");
+for(int rows = 0; rows < count-1; rows++) {
+    Console.Write($"'{newarray[rows]}';");
+   }
+   Console.Write($"'{newarray[count-1]}']");
+}
